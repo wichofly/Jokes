daad3df92b0b3b160e7d8120a3b6e6d10711e0d3
Mauricio Erazo <antonierazo13@hotmail.com>
2022-07-25 10:30:44 +0200

    Using Async Await
    Fetch is a async, so we have to wait until it's done fetching, that's why use await.
    
    whenever we use await inside of a function we need to label that function with "async"

--- script.js
+++ script.js
@@ -5,15 +5,26 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-function generateJoke() {
-    const config = {
-        headers: {
-            'Accept': 'application/json'
+async function generateJoke() {
+        const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
         }
+
+        const res = await fetch('https://icanhazdadjoke.com', config,)
+            
     }
-    fetch('https://icanhazdadjoke.com', config,)
-        .then((res) => res.json())
-        .then((data) => {
-            jokeEl.innerHTML = data.joke
-        })
-}
+
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com', config,)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokeEl.innerHTML = data.joke
+//         })
+// }
