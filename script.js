2c5509288ef7f515adcfd1f329157dc088ae4701
Mauricio Erazo <antonierazo13@hotmail.com>
2022-09-05 00:52:43 +0200

    space 2

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ jokeBtn.addEventListener('click', generateJoke)
 generateJoke()
 
 async function generateJoke() {
-        const config = {
-            headers: {
-                'Accept': 'application/json'
-            }
-        }
+	const config = {
+		headers: {
+			'Accept': 'application/json'
+		}
+	}
 
-        const res = await fetch('https://icanhazdadjoke.com', config)
-        const data = await res.json()
-        jokeEl.innerHTML = data.joke            
-    }
+	const res = await fetch('https://icanhazdadjoke.com', config)
+	const data = await res.json()
+	jokeEl.innerHTML = data.joke
+}
 
 // function generateJoke() {
 //     const config = {
